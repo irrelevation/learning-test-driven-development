d0ea32b19b718cc79c5b3706a92f0c3bb9870cd8
Lukas Zöllner <lks.zoellner@gmail.com>
2022-03-30 15:40:53 +0200

    update file structure and change .mjs to .js files

---
+++ js/src/test.js
@@ -0,0 +1,33 @@
+import { strict as assert } from "assert";
+import { Money } from "./money.js";
+import { Portfolio } from "./portfolio.js";
+
+class MoneyTest {
+  runAllTests() {
+    this.testAddition();
+    this.testDivision();
+    this.testMultiplication();
+  }
+  testMultiplication() {
+    let tenEuros = new Money(10, "EUR");
+    let twentyEuros = new Money(20, "EUR");
+    assert.deepEqual(tenEuros.times(2), twentyEuros);
+  }
+  testDivision() {
+    let initialMoney = new Money(4002, "KRW");
+    let actualMoneyAfterDivision = initialMoney.divideBy(4);
+    let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+    assert.deepEqual(expectedMoneyAfterDivision, actualMoneyAfterDivision);
+  }
+
+  testAddition() {
+    let portfolio = new Portfolio();
+    let fiveDollars = new Money(5, "USD");
+    let tenDollars = new Money(10, "USD");
+    let fifteenDollars = new Money(15, "USD");
+    portfolio.add(fiveDollars, tenDollars);
+    assert.deepEqual(portfolio.evaluate("USD"), fifteenDollars);
+  }
+}
+
+new MoneyTest().runAllTests();
